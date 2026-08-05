323212eaee06d5b86dcb9683588a9d4b56793e03
lidongheng <419337193@qq.com>
2026-08-05 15:06:32 +0800

    20260805下午修复BUG

--- vue.config.js
+++ vue.config.js
@@ -8,20 +8,24 @@ module.exports = defineConfig({
       alias: {
         '@': path.resolve(__dirname, 'src')
       }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          include: path.resolve(__dirname, 'src/icons'),
+          // 覆盖 Vue CLI 默认的 SVG 资源类型，使图标由 svg-sprite-loader 输出。
+          type: 'javascript/auto',
+          use: [
+            {
+              loader: 'svg-sprite-loader',
+              options: {
+                symbolId: 'icon-[name]'
+              }
+            }
+          ]
+        }
+      ]
     }
-  },
-  chainWebpack: (config) => {
-    config.module.rule('svg').exclude.add(path.resolve(__dirname, 'src/icons')).end()
-    config.module
-      .rule('icons')
-      .test(/\.svg$/)
-      .include.add(path.resolve(__dirname, 'src/icons'))
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]'
-      })
-      .end()
   }
 })
